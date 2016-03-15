0b56bf80b49eb8e388eab09cab36ddcc42efdbcc
franky <franky.zhang1994@gmail.com>
2016-03-15 13:23:51 +0800

    add

--- Parser/Baidu_extract_url.py
+++ Parser/Baidu_extract_url.py
@@ -5,6 +5,7 @@ __author__ = 'franky'
 from bs4 import BeautifulSoup
 import urllib2
 import time
+import re
 
 
 def baidu_extract_urls(baidu_serp):
@@ -20,16 +21,19 @@ def baidu_extract_urls(baidu_serp):
                 header = {'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.9; rv:32.0) Gecko/20100101 Firefox/32.0'}
                 req = urllib2.Request(url, None, header)
                 resp = urllib2.urlopen(req, None, 5)
-                url = resp.geturl()
-                result_urls.append(url)
-                print url
+                html = resp.read()
+                soup_ = BeautifulSoup(html)
+                if soup_.title:
+                    url = resp.geturl()
+                    result_urls.append(url)
+                else:
+                    urlp = re.compile(r'<script>window.location.replace\("(.*?)"\)</script>')
+                    url = urlp.search(html).group(1)
+                    result_urls.append(url)
                 break
             except:
                 retry -= 1
-                print 'no', url
                 continue
-        if not retry:
-            result_urls.append('')
 
     return result_urls
 
--- Parser/url_analysis.py
+++ Parser/url_analysis.py
@@ -13,60 +13,40 @@ class url:
         if url_a == '' or url_b == '':
             return 0
         else:
-            try:
-                html_a = urllib2.urlopen(url_a).read()
-                soup_a = BeautifulSoup(html_a)
-                title_a = soup_a.head.title.string.strip()
-                print title_a
-                html_b = urllib2.urlopen(url_b).read()
-                soup_b = BeautifulSoup(html_b)
-                title_b = soup_b.head.title.string.strip()
-                print title_b
-                if title_a == title_b:
-                    return 1
-            except Exception, error:
-                print error
+            if url_a == url_b:
+                return 1
+            else:
                 return 0
 
-    def urlJaccard_and_Kendall(self, baidu_urls, sogou_urls):
+    def urlJaccard(self, baidu_urls, sogou_urls):
         count1 = 0
         l_baidu = len(baidu_urls)
         l_sogou = len(sogou_urls)
         print l_baidu, l_sogou
-        similar_list = []
         for i in range(0, l_baidu):
             for j in range(0, l_sogou):
                 flag = self.urlSimilar(baidu_urls[i], sogou_urls[j])
                 if flag:
                     count1 += 1
-                    if j not in similar_list:
-                        similar_list.append(j)
                     break
         print count1
         jaccard = float(count1)/float(l_sogou + l_baidu - count1)
-        count2 = 0
-        for i in range(0, len(similar_list)):
-            for j in range(i+1, len(similar_list)):
-                if similar_list[i] > similar_list[j]:
-                    count2 += 1
-        print count2
-        tau = 1 - float(count2) * 2 / float(l_baidu * (l_baidu - 1))
-        return [jaccard, tau]
+        return jaccard
 
 
-def compute_Jaccard_and_Kendall(baidu_urls, sogou_urls):
-    return url().urlJaccard_and_Kendall(baidu_urls, sogou_urls)
+def compute_Jaccard(baidu_urls, sogou_urls):
+    return url().urlJaccard(baidu_urls, sogou_urls)
 
 
 def url_analysis():
     queries = open('../query.txt', 'r').readlines()
-    for i in range(0, 1):
+    for i in range(0, 10):
         query = queries[i].strip()
         baidu_serp = open('../SERP_baidu/' + query + '_baidu.html', 'r').read()
         sogou_serp = open('../SERP_sogou/' + query + '_sogou.html', 'r').read()
         baidu_urls = baidu_extract_urls(baidu_serp)
         sogou_urls = sogou_extract_urls(sogou_serp)
-        print query, compute_Jaccard_and_Kendall(baidu_urls, sogou_urls)
+        print query, compute_Jaccard(baidu_urls, sogou_urls)
 
 if __name__ == '__main__':
     url_analysis()
